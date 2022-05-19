c44bc28c57204d477c70a148e32446e8d8315940
Elliott Smith <smite426@hsd.k12.or.us>
2022-05-19 09:52:55 -0700

    added scripts.js and made menu icon

---
+++ scripts.js
@@ -0,0 +1,37 @@
+var mobileBreakpoint = 600;
+
+window.onload = function() {
+    var screenWidth = window.innerWidth;
+    var globalNavbar = document.getElementById("global-nav");
+    var toggle = document.getElementById("menu-toggle");
+
+    if (screenWidth > mobileBreakpoint) {
+        globalNavbar.classList.add("open");
+        toggle.classList.add("open");
+    }
+
+    toggle.onclick = function() {
+        if (globalNavbar.classList.contains("open")){
+            globalNavbar.classList.remove("open");
+        }
+        else {
+            globalNavbar.classList.add("open");
+        }
+    }
+
+}
+
+window.onresize = function() {
+    var screenWidth = window.innerWidth;
+    var globalNavbar = document.getElementById("global-nav");
+    var toggle = document.getElementById("menu-toggle");
+
+    if (screenWidth > mobileBreakpoint) {
+        globalNavbar.classList.add("open");
+        toggle.classList.add("open");
+    }
+    else {
+        globalNavbar.classList.remove("open")
+        toggle.classList.remove("open");
+    }
+}
